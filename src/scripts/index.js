36b48fbdf598ddb53cc6978a940253aeda71968d
Cleyton Caminha <cleytoncaminha01@gmail.com>
2021-11-09 01:03:54 -0300

    mostrar dados pesquisados na tela

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,21 @@
-async function user(){
-    const response = await fetch('https://api.github.com/users/cadudias')
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName)
+})
+
+async function user(userName){
+    const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json()
 }
 
-console.log(await user())
+function getUserProfile(userName){
+    user(userName).then(UserData => {
+        let userInfo = `<img src="${UserData.avatar_url}" alt="foto do perfil do usuario" />
+        <div class="data">
+        <h1>${UserData.name ?? 'Não possui nome cadastrado 😥'}</h1>
+        <p>${UserData.bio ?? 'Não possui bio cadastrado 😥'}</p>
+        </div>
+        `
+        document.querySelector('.profile-data').innerHTML = userInfo
+    })
+}
